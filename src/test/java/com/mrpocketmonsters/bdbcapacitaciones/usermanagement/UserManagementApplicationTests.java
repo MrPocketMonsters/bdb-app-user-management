cd8f795f48aa141246595dc52564f17d7cad846b
Nicolas Sabogal <ndsabogalv@gmail.com>
2025-10-25 17:29:43 -0500

    chore: first commit

---
+++ src/test/java/com/mrpocketmonsters/bdbcapacitaciones/usermanagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrpocketmonsters.bdbcapacitaciones.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
